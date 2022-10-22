d1bc9a7ff23bbb737b5aaee5106bdd06c9118a4a
Rainigeria <okoroaforgeorgebethel@gmail.com>
2022-10-22 14:04:23 +0100

    Added delete and search functions to linkedlist

--- Singlylinked_list.cpp
+++ Singlylinked_list.cpp
@@ -2,73 +2,141 @@
 
 class Node{
 
-public:
-    int data;
+        public:
+            int data;
 
-    Node* next;
+            Node* next;
 
-    Node(){
+            Node(){
 
-    this->data = 0;
-    
-    this->next = NULL;
-    
-    }
+            this->data = 0;
+            
+            this->next = NULL;
+            
+          }
 
-    Node(int data){
+            Node(int data){
 
-    this->data = data;
-    this->next = NULL;
+            this->data = data;
+            this->next = NULL;
 
-    }
+          }
 
 };
 
 
 class LinkList{
 
-public:
-    Node* head;
+            public:
+                Node* head;
 
-    LinkList(){head = NULL;}
+                LinkList(){head = NULL;}
 
-    void append(int);
+                void append(int);
 
-    void insert(int);
+                void insert(int);
 
-    void sort();
+                Node* search(int);
 
-    void Del();
+                void Delete(int);
 
-    void prepend();
+                void prepend();
 
-    void printList();
+                void printList();
 
-};
+            };
 
-void LinkList::append(int data){
+void LinkList::Delete(int data){
 
-    Node* newNode = new Node(data);
+      if(head == NULL){
 
-      while (head == NULL)
-      {
-        head = newNode;
+        std::cout << "list is empty" << std::endl;
 
         return;
 
       }
 
-    Node* temp = head;
+      else{
 
-    while(temp->next != NULL){
+         Node* temp = head;
+         Node* prev;
 
-        temp = temp->next;
+         while(temp->next != NULL && temp->data != data){
 
-    } 
+            prev = temp;
 
-    temp->next = newNode;
+            temp = temp->next;
 
-    }
+         }
+
+         std::cout<<"data prev: "<<prev->data<<", pointer: "<<prev->next<<std::endl;
+
+         prev->next = temp->next;
+
+         delete temp;
+
+         temp = NULL;
+
+
+      }
+
+}
+
+Node* LinkList::search(int data){
+
+      Node* location;
+
+      if(head == NULL){
+
+       std::cout<<"List is empty"<<std::endl;
+
+       return NULL;
+
+      }
+
+      else{
+
+
+       Node* temp = head;
+
+       while(temp != NULL && temp ->data != data){
+
+          temp = temp->next;
+
+       }
+
+       location = temp->next;
+
+       return location;
+
+      }
+
+
+}
+
+void LinkList::append(int data){
+
+        Node* newNode = new Node(data);
+
+          while (head == NULL)
+          {
+            head = newNode;
+
+            return;
+
+          }
+
+        Node* temp = head;
+
+        while(temp->next != NULL){
+
+            temp = temp->next;
+
+        } 
+
+        temp->next = newNode;
+
+        }
 
 void LinkList::printList(){
 
@@ -78,10 +146,12 @@ void LinkList::printList(){
 
     while (temp != NULL){
         
-        std::cout<<temp->data<<std::endl;
+        std::cout<<"data: "<<temp->data<<", pointer: "<<temp->next<<std::endl;
         
         temp = temp->next;
     }
+
+
 
 }
 
@@ -94,8 +164,17 @@ int main(){
   llst.append(20);
   llst.append(6);
   llst.append(8);
+  
 
   llst.printList();
+
+  llst.Delete(20);
+
+  llst.printList();
+
+  // Node* location = llst.search(10);
+
+  // std::cout<<"pointer location of data 10: "<< location <<std::endl;
 
 
 }
